32334c97b70ea27ac5bd7dcf5e4ec639e06a9b87
MartinKayz <kubonamartinyafesi132@gmail.com>
2023-02-09 13:50:22 +0300

    Trial code pushed

--- Proj_1_Covid/src/controller/menu.py
+++ Proj_1_Covid/src/controller/menu.py
@@ -7,6 +7,7 @@ sys.path.append("../view/")
 from Covid_19 import COVID19Uganda
 from district import District
 from visuals import Visuals
+import pandas as pd
 
 def main():
     covid_data = COVID19Uganda()
@@ -17,8 +18,8 @@ def main():
         '3': covid_data.retrieve_total_cases,
         '4': covid_data.retrieve_average_cases,
         '5': covid_data.visualize_data,
-        '6': covid_data.load_data_from_file,
-        '7': covid_data.save_data_to_file,
+        # '6': covid_data.load_data_from_file,
+        # '7': covid_data.save_data_to_file,
         'Q': quit
     }
 
@@ -36,13 +37,12 @@ def main():
         choice = input("Enter choice: ").upper()
 
         if choice in choices:
-            if choice == '1':
+            if choice == '1': 
                 district_name = input("Enter district name: ")
                 num_of_confirmed_cases = int(input("Enter number of confirmed cases: "))
                 num_of_hospitalizations = int(input("Enter number of hospitalizations: "))
                 num_of_deaths = int(input("Enter number of deaths: "))
-                district = District(district_name,num_of_confirmed_cases,num_of_hospitalizations ,num_of_deaths)
-                choices[choice](district)
+                choices[choice](district_name, num_of_confirmed_cases, num_of_hospitalizations, num_of_deaths)
             elif choice == '2':
                 district_name = input("Enter district name: ")
                 district_data = choices[choice](district_name)
@@ -54,7 +54,7 @@ def main():
                 else:
                     print("District not found.")
             elif choice in ['3', '4']:
-                data = choices[choice]()
+                data = choices[choice](0,0,0)
                 print(f"{'Total' if choice == '3' else 'Average'} Confirmed Cases: ", data[0])
                 print(f"{'Total' if choice == '3' else 'Average'} Hospitalizations: ", data[1])
                 print(f"{'Total' if choice == '3' else 'Average'} Deaths: ", data[2])
