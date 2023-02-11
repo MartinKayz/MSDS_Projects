c371ee1c4ccf45f304b22521f8907124c020f4f9
MartinKayz <kubonamartinyafesi132@gmail.com>
2023-02-11 14:20:16 +0300

    Fixed Default functions and menus

--- Proj_1_Covid/src/controller/menu.py
+++ Proj_1_Covid/src/controller/menu.py
@@ -1,4 +1,4 @@
-import sys
+import sys, csv
 
 sys.path.append("../model/")
 sys.path.append("../view/")
@@ -9,28 +9,22 @@ from district import District
 covid = COVID19Uganda()
 
 while True:
-    print("Enter 1 to add a district, 2 to retrieve district information, 3 to retrieve total statistics, 4 to retrieve average statistics, or Q to exit.")
-    choice = int(input("Enter your choice: "))
+    print("Enter\n 1 to add a district,\n 2 to retrieve district information,\n 3 to retrieve total statistics,\n 4 to retrieve average statistics,\n or Q to exit.")
+    choice = input("Enter your choice: ")
 
-    if choice == "Q":
+    if choice == "Q" or 'q':
         break
-    elif choice == 1:
-        name = input("Enter district name: ")
-        confirmed_cases = int(input("Enter number of confirmed cases: "))
-        hospitalizations = int(input("Enter number of hospitalizations: "))
-        deaths = int(input("Enter number of deaths: "))
-
-        district = District(name, confirmed_cases, hospitalizations, deaths)
-        covid.add_district(district)
-    elif choice == 2:
+    elif choice == '1':
+        covid.add_district()
+    elif choice == '2':
         name = input("Enter district name: ")
         print(covid.get_district_info(name))
-    elif choice == 3:
+    elif choice == '3':
         print("Total cases:", covid.get_total_cases())
         print("Total hospitalizations:", covid.get_total_hospitalizations())
         print("Total deaths:", covid.get_total_deaths())
-    elif choice == 4:
-        print("Average cases:", covid.get_average_cases())
+    elif choice == '4':
+        print("Average cases:", covid.get_average_data())
         print("Average hospitalizations:", covid.get_average_hospitalizations())
         print("Average deaths:", covid.get_average_deaths())
     else:
